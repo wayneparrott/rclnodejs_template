d57565740168d48aa930ad294cb61238865dfd0a
wayne <wayne@dev-linux-18.04>
2019-11-13 07:25:25 -0600

    added missing -d arg for rviz2 & example static_transform_publisher

--- install_support/launchtemplate.launch.py
+++ install_support/launchtemplate.launch.py
@@ -5,9 +5,12 @@ def generate_launch_description():
   return LaunchDescription([
     launch_ros.actions.Node(
       package='_PKG_', node_executable='_EXECUTABLE_', output='screen'),
-   launch_ros.actions.Node(
+    launch_ros.actions.Node(
+      package='ros_tf2', node_executable='static_transform_publisher', output='screen',
+      arguments=[0, 0, 1, 0, 0, 0, 'Temperature', 'map']),
+    launch_ros.actions.Node(
       package='rviz2', 
       node_executable='rviz2', 
       output='screen',
-      arguments=['_RVIZ_CONFIG_PATH_'])
+      arguments=['-d', _RVIZ_CONFIG_PATH_'])
   ])
